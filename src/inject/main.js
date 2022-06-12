7fa9568e36f2433558fc7ed76e676d4cee6d28ba
ridoy <majumdar.ridoy@gmail.com>
2022-06-12 11:09:24 -0400

    Add documentation and retry method for display injection

--- src/inject/main.js
+++ src/inject/main.js
@@ -1,29 +1,12 @@
-console.log("Script loaded");
+// @name        Pokemon Showdown God Mode 
+// @author      github.com/ridoy
+// @description What's the most damage my opponent can do to me next turn? And me to them? This script answers 
+//              those questions by calculating and displaying damage ranges of both side's moves each turn. 
+//              Currently only configured to work for Generation 7 Random Battles.
 
-// Embed damage calculation script + random battles data within local context.
-// These are available locally as `calc` and `gen7FormatsData` respectively.
-embedScript(smogonCalcData, "data-script");
-embedScript(smogonCalc, "calc-script");
-embedScript(gen7FormatsData, "randbats-moves");
-
-
-
-let damageCalculatorInitScript = "let damageCalculator = new DamageCalculator();"
-embedScript(DamageCalculator.toString() + damageCalculatorInitScript, "damage-calculator-script");
-
-// Redisplay damage calculations if move is canceled
-// document.querySelector(".button[name='undoChoice']").addEventListener("click", calculateDamageBothSides);
-
-// TODO reset currentTurn when battle is over or tab is closed.
-let currentTurn = $('h2.battle-history').length;
-
-// Execution begins here.
-setInterval(checkIfNewTurn, 1000);
-
-// Damage calculation object. This gets embedded as a new <script> into the page in order to access the local context,
-// which is necessary for accessing local variables like `app` which contain vital data about the current 
-// game state. The local state cannot be accessed from within this Chrome extension script (separate contexts) 
-// so we must embed.
+// Damage calculation object. This is embedded as a <script> into the page so we can access local variables like
+// `app` which contains the game state. This cannot be accessed otherwise: Chrome extensions work in a separate
+// scope from the page.
 function DamageCalculator() {
     let $this = this;
     this.calculateDamages = function(gen, moves, attacker, defender) {
@@ -52,24 +35,12 @@ function DamageCalculator() {
     this.getGeneration = function() { // TODO add support for other generations
         return calc.Generations.get(7);
     };
-    this.run = function() {
-        let gen = $this.getGeneration();
-        let myPkmnName = app.curRoom.battle.mySide.active[0].speciesForme;
-        myTeam = app.curRoom.battle.myPokemon;
-        myPkmn = myTeam.filter((pkmn) => pkmn.speciesForme === myPkmnName)[0];
-        theirPkmn = app.curRoom.battle.farSide.active[0];
-        theirPkmnNameFormatted = theirPkmn
-            .speciesForme
-            .replaceAll("-","")
-            .replaceAll(" ", "")
-            .toLowerCase();
-        opponentMoves = gen7FormatsData[theirPkmnNameFormatted]["randomBattleMoves"];
-        myPkmnObj = $this.initPokemon(gen, myPkmn);
-        theirPkmnObj = $this.initPokemon(gen, theirPkmn);
-        yourDamageText = $this.calculateDamages(gen, myPkmn.moves, myPkmnObj, theirPkmnObj);
-        oppDamageText = $this.calculateDamages(gen, opponentMoves, theirPkmnObj, myPkmnObj);
-        if (!document.getElementById("damage-display")) {
-            damageDisplay = document.createElement("div");
+    this.displayDamages = function(yourDamages, theirDamages) {
+        try {
+            let previouslyInjectedDisplay = document.getElementById("damage-display");
+            if (previouslyInjectedDisplay) previouslyInjectedDisplay.remove();
+        
+            let damageDisplay = document.createElement("div");
             damageDisplayLeft = document.createElement("div");
             damageDisplayRight = document.createElement("div");
             damageDisplay.id = "damage-display";
@@ -77,10 +48,44 @@ function DamageCalculator() {
             damageDisplayRight.id = "damage-display-right";
             damageDisplay.appendChild(damageDisplayLeft);
             damageDisplay.appendChild(damageDisplayRight);
+            damageDisplayLeft.innerText = "Your moves:\n" + yourDamages.join("\n");
+            damageDisplayRight.innerText = "Their (possible) moves:\n" + theirDamages.join("\n");
+            document.getElementsByClassName("controls")[0].appendChild(damageDisplay);
+            return true;
+        } catch (e) {
+            console.log(e);
         }
-        damageDisplayLeft.innerText = "Your moves:\n" + yourDamageText.join("\n");
-        damageDisplayRight.innerText = "Their (possible) moves:\n" + oppDamageText.join("\n");
-        document.getElementsByClassName("controls")[0].appendChild(damageDisplay);
+    };
+    this.retryIfFail = function(func, interval, i) {
+       if (!i) i = 0;
+       if (i > 5) {
+           return; // 5 is max retries for now
+       }
+       setTimeout(function() {
+           var success = func();
+           if (!success) retryIfFail(func, interval, i + 1);
+       }, interval);
+   } 
+    // where I left off: weird bug where script does not read team initially. 
+    this.run = function() {
+        let gen = $this.getGeneration();
+        let myPkmnName = app.curRoom.battle.mySide.active[0].speciesForme;
+        let myBoosts = app.curRoom.battle.mySide.active[0].boosts;
+        let myTeam = app.curRoom.battle.myPokemon;
+        let myPkmn = myTeam.filter((pkmn) => pkmn.speciesForme === myPkmnName)[0];
+        myPkmn.boosts = myBoosts;
+        let theirPkmn = app.curRoom.battle.farSide.active[0];
+        let theirPkmnNameFormatted = theirPkmn
+            .speciesForme
+            .replaceAll("-","")
+            .replaceAll(" ", "")
+            .toLowerCase();
+        let theirMoves = gen7FormatsData[theirPkmnNameFormatted]["randomBattleMoves"];
+        let myPkmnObj = $this.initPokemon(gen, myPkmn);
+        let theirPkmnObj = $this.initPokemon(gen, theirPkmn);
+        let yourDamages = $this.calculateDamages(gen, myPkmn.moves, myPkmnObj, theirPkmnObj);
+        let theirDamages = $this.calculateDamages(gen, theirMoves, theirPkmnObj, myPkmnObj);
+        $this.retryIfFail($this.displayDamages.bind($this, yourDamages, theirDamages), 1000);
     }
 }
 
@@ -113,3 +118,23 @@ function embedScript(scriptAsString, id) {
     s.textContent = scriptAsString;
     (document.head).appendChild(s);
 }
+
+console.log("Script loaded");
+
+// Embed damage calculation script + random battles data within local context.
+// These are available locally as `calc` and `gen7FormatsData` respectively.
+embedScript(smogonCalcData, "data-script");
+embedScript(smogonCalc, "calc-script");
+embedScript(gen7FormatsData, "randbats-moves");
+
+let damageCalculatorInitScript = "let damageCalculator = new DamageCalculator();"
+embedScript(DamageCalculator.toString() + damageCalculatorInitScript, "damage-calculator-script");
+
+// Redisplay damage calculations if move is canceled
+// document.querySelector(".button[name='undoChoice']").addEventListener("click", calculateDamageBothSides);
+
+// TODO reset currentTurn when battle is over or tab is closed.
+let currentTurn = $('h2.battle-history').length;
+
+// Execution begins here.
+setInterval(checkIfNewTurn, 1000);
