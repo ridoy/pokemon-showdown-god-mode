10572adf9e3ae5ff2c0e784674a78d17d31c609c
ridoy <majumdar.ridoy@gmail.com>
2022-10-30 22:00:00 -0400

    Make slideout actually slide out

---
+++ issue-tracker.md
@@ -0,0 +1,18 @@
+# Issue Tracker
+
+Because using the Github issue tracker feels too fancy for now.
+
+## Functionality
+~~F1 Adjustable slideout for damage container~~
+F2 Elegant UI for considering possible enemy items (Use a warning message for now: "damage ranges may be higher or lower than expected depending on the enemy's held item)
+F3 Elegant UI for entire matrix of matchups
+
+
+## Bugs
+B1 Regional form bugs
+B2 include Z moves in calculations
+B3 Slideout messes up when battle starts
+
+## Maintenance
+
+M1 Why am I not using any jquery lol
--- src/inject/main.js
+++ src/inject/main.js
@@ -9,24 +9,37 @@ const createSlideoutContainer = function() {
     damageDisplaySlideout.id = "damage-display-slideout";
 
     let damageDisplayCollapseButton = document.createElement("span");
-    damageDisplayCollapseButton.innerText = "Collapse / Expand [x] ";
+    damageDisplayCollapseButton.innerText = "Click and drag to adjust size";
     damageDisplayCollapseButton.id = "collapse-button";
-
 
     damageDisplaySlideout.appendChild(damageDisplayCollapseButton);
     document.body.appendChild(damageDisplaySlideout);
 
-    damageDisplayCollapseButton.addEventListener('click', function() {
-        this.classList.toggle("active");
-        console.log("Collapse button clicked.");
-        let content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
+    let dragging = false;
+    const SLIDEOUT_MIN_HEIGHT = 50; // in px
+    $(damageDisplaySlideout).css("top", $(window).height() - SLIDEOUT_MIN_HEIGHT);
+    $(damageDisplayCollapseButton).mousedown(function(e) {
+        let dragging = true;
+        $(damageDisplayCollapseButton).css("cursor", "grabbing");
+        $(document).mousemove(function(e) {
+            let cursorTooLow = $(window).height() - e.pageY < SLIDEOUT_MIN_HEIGHT;
+            let cursorTooHigh = e.pageY < 0;
+            if (cursorTooLow) {
+                $(damageDisplaySlideout).css("top", $(window).height() - SLIDEOUT_MIN_HEIGHT);
+            } else if (cursorTooHigh) {
+                $(damageDisplaySlideout).css("top", 0);
+            } else {
+                $(damageDisplaySlideout).css("top", e.pageY);
+            }
+        });
+    })
+    $(document).mouseup(function() {
+        dragging = false;
+        $(damageDisplayCollapseButton).css("cursor", "grab");
+        $(document).unbind('mousemove');
+    })
 }
+
 
 createSlideoutContainer();
 
